9b8aeffd7e45ee3492e23978faadd4fbec682cf6
Raun Gray <raungray@gmail.com>
2020-09-14 13:24:45 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import {NgModule} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+})
+@NgModule ({
+  imports: [MatSliderModule]
 })
 export class AppComponent {
   title = 'newtron';
